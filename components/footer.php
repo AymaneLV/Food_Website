38010c28f08bb6ff61bea3b602e2436246055762
AymaneLV <themrvvvv@gmail.com>
2024-12-31 23:08:54 +0100

    Initial commit

---
+++ components/footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:aymanlaffigh@gmail.com">aymanlaffigh@gmail.com</a>
+         <a href="mailto:achrafelbitari627@gmail.com">achrafelbitari627@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>07:00am to 23:30pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">Meknes, Morocco - 50000</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:0653912165">+212 0653-912165</a>
+         <a href="tel:0656655461">+212 0656-655461</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>AymaneVV</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
